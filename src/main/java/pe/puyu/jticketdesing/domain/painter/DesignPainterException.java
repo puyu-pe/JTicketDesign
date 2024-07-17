286cdb07571c0d7977f61514e3a1a760992dfc8c
OscarDevCPP <oscarchh0799@gmail.com>
2024-07-17 11:25:58 -0500

    Add DesignPainterException
    issue #35

---
+++ src/main/java/pe/puyu/jticketdesing/domain/painter/DesignPainterException.java
@@ -0,0 +1,8 @@
+package pe.puyu.jticketdesing.domain.painter;
+
+public class DesignPainterException extends RuntimeException{
+
+    public DesignPainterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
